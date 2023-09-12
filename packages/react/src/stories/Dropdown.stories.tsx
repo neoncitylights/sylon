d162048d058bce7be7ce898bec9142a9772d49f2
Samantha <sam.t.nguyenn@gmail.com>
2023-09-11 22:37:50 -0500

    chore: fix minor formatting/whitespace issues

--- packages/react/src/stories/Dropdown.stories.tsx
+++ packages/react/src/stories/Dropdown.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
-
-import { Dropdown, DropdownItem, DropdownItemKbd } from '../components/';
 import { IconCommand } from '@tabler/icons-react';
+import { Dropdown, DropdownItem, DropdownItemKbd } from '../components/';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof Dropdown> = {
@@ -18,18 +17,18 @@ export const Primary: Story = {
 	args: {
 		children: <>
 			<DropdownItem>
-            Item 1
-            <DropdownItemKbd>
-              <IconCommand width={12} height={12} />F
-            </DropdownItemKbd>
-          </DropdownItem>
-          <DropdownItem>
-            Item 2
-            <DropdownItemKbd>
-              <IconCommand width={12} height={12} />G
-            </DropdownItemKbd>
-          </DropdownItem>
-          <DropdownItem>Item 3</DropdownItem>
+						Item 1
+						<DropdownItemKbd>
+							<IconCommand width={12} height={12} />F
+						</DropdownItemKbd>
+					</DropdownItem>
+					<DropdownItem>
+						Item 2
+						<DropdownItemKbd>
+							<IconCommand width={12} height={12} />G
+						</DropdownItemKbd>
+					</DropdownItem>
+					<DropdownItem>Item 3</DropdownItem>
 		</>
 	},
 };
