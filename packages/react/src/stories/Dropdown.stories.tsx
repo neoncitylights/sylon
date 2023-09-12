51ca22de99a5801757c4c08ba8b624fd0f8ab5d7
Samantha <sam.t.nguyenn@gmail.com>
2023-09-11 21:17:49 -0500

    chore: create Dropdown component w/ story

---
+++ packages/react/src/stories/Dropdown.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Dropdown, DropdownItem, DropdownItemKbd } from '../components/';
+import { IconCommand } from '@tabler/icons-react';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta: Meta<typeof Dropdown> = {
+	title: 'Components/Dropdown',
+	component: Dropdown,
+	tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof Dropdown>
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+	args: {
+		children: <>
+			<DropdownItem>
+            Item 1
+            <DropdownItemKbd>
+              <IconCommand width={12} height={12} />F
+            </DropdownItemKbd>
+          </DropdownItem>
+          <DropdownItem>
+            Item 2
+            <DropdownItemKbd>
+              <IconCommand width={12} height={12} />G
+            </DropdownItemKbd>
+          </DropdownItem>
+          <DropdownItem>Item 3</DropdownItem>
+		</>
+	},
+};
