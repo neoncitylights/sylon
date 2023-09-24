982e9a50198db958254f935e198a99cfba7dad0e
Samantha <contact@samanthanguyen.me>
2023-09-24 17:25:22 -0500

    chore: upgrade storybook, setup for monorepo (#15)
    - framework.options.postCSS removed, since Vite comes preconfigured with PostCSS support

--- packages/react/.storybook/main.ts
+++ packages/react/.storybook/main.ts
@@ -1,27 +1,31 @@
+import { dirname, join } from 'path';
 import { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
+	stories: [
+		"../src/**/*.mdx",
+		"../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+	],
+	addons: [
+		getAbsolutePath("@storybook/addon-links"),
+		getAbsolutePath("@storybook/addon-essentials"),
+		getAbsolutePath("@storybook/addon-onboarding"),
+		getAbsolutePath("@storybook/addon-interactions"),
 		{
 			name: "@storybook/addon-styling",
-			options: {
-				postCss: {
-					implementation: require.resolve('postcss'),
-				}
-			}
+			options: {}
 		}
-  ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+	],
+	framework: {
+		name: "@storybook/react-vite",
+		options: {},
+	},
+	docs: {
+		autodocs: "tag",
+	},
 };
 export default config;
+
+function getAbsolutePath(value: string): string {
+	return dirname(require.resolve(join(value, "package.json")));
+}
