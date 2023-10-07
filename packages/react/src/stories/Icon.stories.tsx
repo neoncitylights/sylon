c00c271a2ad519fe9021fa385aed93c57cf57990
Samantha <sam.t.nguyenn@gmail.com>
2023-10-07 17:24:17 -0500

    chore: add root import, add story for Icon component

---
+++ packages/react/src/stories/Icon.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Icon } from '../components/Icon';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta: Meta<typeof Icon> = {
+	title: 'Components/Icon',
+	component: Icon,
+	tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof Icon>
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+	args: {
+		name: '2fa'
+	},
+};
