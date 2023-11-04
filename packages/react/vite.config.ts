b993aa109856d4f6c67b4e0b8302e38556256626
Samantha <contact@samanthanguyen.me>
2023-11-03 22:43:25 -0500

    chore(build): partially revert 18b07c0 to fix compiler warning (#81)
    - fixes an error with TypeScript saying that React is a UMD global
    - drops the UMD format from the Vite
    - fixes ESBuild error with unrecognized directive (see below)
    
    ESBuild doesn't recognize the 'use client' directive, which will cause the following error when compiling:
    
    ```
    src/components/Button/ToggleButton.tsx (1:0) Error when using sourcemap for reporting an error: Can't resolve original location of error.
    ```

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
 		lib: {
 			entry: path.resolve(__dirname, 'src/index.ts'),
 			name: 'sylon',
+			formats: ['es'],
 			fileName: (format) => `sylon.${format}.js`,
 		},
 	},
