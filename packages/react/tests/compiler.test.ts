c5c53753a55ed384674fddbcc5b6dd6454d866a1
Samantha <contact@samanthanguyen.me>
2023-11-05 15:50:18 -0600

    tests: group tests by config file, test for JSX output

--- packages/react/tests/compiler.test.ts
+++ packages/react/tests/compiler.test.ts
@@ -1,52 +1,60 @@
 import { ExternalOption, OutputOptions } from 'rollup';
 import { LibraryOptions } from 'vite';
-import { assertType,expect, test } from 'vitest';
+import { assertType, describe, expect, test } from 'vitest';
 
 import tsConfig from './../tsconfig.json';
 import viteConfig from './../vite.config.ts';
 
-test('assert moduleResolution is "Bundler"', () => {
-	expect(tsConfig.compilerOptions.moduleResolution).toBe('Bundler');
-});
+describe('TypeScript config', () => {
+	test('assert moduleResolution is "Bundler"', () => {
+		expect(tsConfig.compilerOptions.moduleResolution).toBe('Bundler');
+	});
 
-test('assert Storybook files are not compiled', () => {
-	expect(tsConfig.exclude).toContain('src/**/*.stories.*');
-});
+	test('assert Storybook files are not compiled', () => {
+		expect(tsConfig.exclude).toContain('src/**/*.stories.*');
+	});
 
-test('assert compiler output is ESM', () => {
-	const lib = viteConfig.build?.lib as LibraryOptions;
-	expect(lib.formats).toContain('es');
-});
+	test('assert compiler output is ESM', () => {
+		const lib = viteConfig.build?.lib as LibraryOptions;
+		expect(lib.formats).toContain('es');
+	});
 
-test.each([
-	['tailwindcss'],
-	['react'],
-	['react-dom'],
-	['react/jsx-runtime'],
-])('assert peer dependency "%s" is excluded', async (dependency) => {
-	const { build } = viteConfig;
-	const externalDeps = build?.rollupOptions?.external;
+	test('assert JSX construct uses React 17 transform', () => {
+		expect(tsConfig.compilerOptions.jsx).toBe('react-jsx');
+	})
+})
 
-	expect(build?.rollupOptions?.external).not.toBeUndefined();
-	expect(build?.rollupOptions?.external).not.toBe([]);
-	assertType<ExternalOption[]>(externalDeps as any);
+describe("Vite config", () => {
+	test.each([
+		['tailwindcss'],
+		['react'],
+		['react-dom'],
+		['react/jsx-runtime'],
+	])('assert peer dependency "%s" is excluded', async (dependency) => {
+		const { build } = viteConfig;
+		const externalDeps = build?.rollupOptions?.external;
 
-	expect(externalDeps).toContain(dependency);
-});
+		expect(build?.rollupOptions?.external).not.toBeUndefined();
+		expect(build?.rollupOptions?.external).not.toBe([]);
+		assertType<ExternalOption[]>(externalDeps as any);
 
-test.each([
-	['react', 'React'],
-	['tailwindcss', 'tailwindcss'],
-])('assert "%s" is setup as global with Rollup', async (dep, global) => {
-	const { build } = viteConfig;
-	const output = build?.rollupOptions?.output as OutputOptions;
-	const globals = output?.globals;
+		expect(externalDeps).toContain(dependency);
+	});
 
-	expect(globals).not.toBeUndefined();
-	expect(globals).not.toBe({});
-	assertType<Record<string, string>>(globals as any);
+	test.each([
+		['react', 'React'],
+		['tailwindcss', 'tailwindcss'],
+	])('assert "%s" is setup as global with Rollup', async (dep, global) => {
+		const { build } = viteConfig;
+		const output = build?.rollupOptions?.output as OutputOptions;
+		const globals = output?.globals;
 
-	// look at each key-value pair
-	expect(globals).toHaveProperty(dep);
-	expect(globals?.[dep]).toBe(global);
-});
+		expect(globals).not.toBeUndefined();
+		expect(globals).not.toBe({});
+		assertType<Record<string, string>>(globals as any);
+
+		// look at each key-value pair
+		expect(globals).toHaveProperty(dep);
+		expect(globals?.[dep]).toBe(global);
+	});
+})
