64dcbbf86f499416b58267428c11ad9e027ae507
Samantha <contact@samanthanguyen.me>
2023-11-05 16:24:49 -0600

    tests: +regression tests for compiler setup, cleanup  (#88)
    - tests for library types included
    - (more) tests for excluded directories
    - group tests by config file

--- packages/react/tests/compiler.test.ts
+++ packages/react/tests/compiler.test.ts
@@ -1,52 +1,80 @@
 import { ExternalOption, OutputOptions } from 'rollup';
 import { LibraryOptions } from 'vite';
-import { assertType,expect, test } from 'vitest';
+import { assertType, describe, expect, test } from 'vitest';
 
 import tsConfig from './../tsconfig.json';
 import viteConfig from './../vite.config.ts';
 
-test('assert moduleResolution is "Bundler"', () => {
-	expect(tsConfig.compilerOptions.moduleResolution).toBe('Bundler');
+describe('TypeScript config', () => {
+	describe('assert TS compiler setup for Vite is correct', () => {
+		const { compilerOptions } = tsConfig;
+
+		test.each([
+			['moduleResolution', 'Bundler'],
+			['module', 'ESNext'],
+			['noEmit', true],
+		])('assert %s is set to %s', (key, value) => {
+			expect(compilerOptions[key]).toBe(value);
+		});
+	});
+
+	test.each([
+		['src/**/*.stories.*', 'Storybook'],
+		['node_modules', 'Node modules'],
+		['dist', 'Compiled output'],
+	])('assert "%s" directory (%s) is not compiled', (directory) => {
+		expect(tsConfig.exclude).toContain(directory);
+	});
+
+	test('assert JSX construct uses React 17 transform', () => {
+		expect(tsConfig.compilerOptions.jsx).toBe('react-jsx');
+	});
+
+	test.each([
+		['ES2022'],
+		['DOM'],
+		['DOM.Iterable'],
+	])('type definitions for built-in JS APIS includes %s', (libType) => {
+		expect(tsConfig.compilerOptions.lib).toContain(libType);
+	});
 });
 
-test('assert Storybook files are not compiled', () => {
-	expect(tsConfig.exclude).toContain('src/**/*.stories.*');
-});
+describe('Vite config', () => {
+	test('assert compiler output is ESM', () => {
+		const lib = viteConfig.build?.lib as LibraryOptions;
+		expect(lib.formats).toContain('es');
+	});
 
-test('assert compiler output is ESM', () => {
-	const lib = viteConfig.build?.lib as LibraryOptions;
-	expect(lib.formats).toContain('es');
-});
+	test.each([
+		['tailwindcss'],
+		['react'],
+		['react-dom'],
+		['react/jsx-runtime'],
+	])('assert peer dependency "%s" is excluded', (dependency) => {
+		const { build } = viteConfig;
+		const externalDeps = build?.rollupOptions?.external;
 
-test.each([
-	['tailwindcss'],
-	['react'],
-	['react-dom'],
-	['react/jsx-runtime'],
-])('assert peer dependency "%s" is excluded', async (dependency) => {
-	const { build } = viteConfig;
-	const externalDeps = build?.rollupOptions?.external;
+		expect(build?.rollupOptions?.external).not.toBeUndefined();
+		expect(build?.rollupOptions?.external).not.toBe([]);
+		assertType<ExternalOption[]>(externalDeps as any);
 
-	expect(build?.rollupOptions?.external).not.toBeUndefined();
-	expect(build?.rollupOptions?.external).not.toBe([]);
-	assertType<ExternalOption[]>(externalDeps as any);
+		expect(externalDeps).toContain(dependency);
+	});
 
-	expect(externalDeps).toContain(dependency);
-});
+	test.each([
+		['react', 'React'],
+		['tailwindcss', 'tailwindcss'],
+	])('assert "%s" is setup as global with Rollup', (dependency, global) => {
+		const { build } = viteConfig;
+		const output = build?.rollupOptions?.output as OutputOptions;
+		const globals = output?.globals;
 
-test.each([
-	['react', 'React'],
-	['tailwindcss', 'tailwindcss'],
-])('assert "%s" is setup as global with Rollup', async (dep, global) => {
-	const { build } = viteConfig;
-	const output = build?.rollupOptions?.output as OutputOptions;
-	const globals = output?.globals;
+		expect(globals).not.toBeUndefined();
+		expect(globals).not.toBe({});
+		assertType<Record<string, string>>(globals as any);
 
-	expect(globals).not.toBeUndefined();
-	expect(globals).not.toBe({});
-	assertType<Record<string, string>>(globals as any);
-
-	// look at each key-value pair
-	expect(globals).toHaveProperty(dep);
-	expect(globals?.[dep]).toBe(global);
+		// look at each key-value pair
+		expect(globals).toHaveProperty(dependency);
+		expect(globals?.[dependency]).toBe(global);
+	});
 });
