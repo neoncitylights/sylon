a1412803b07e0ba5ac0198d479295ddb1d36de48
Samantha <contact@samanthanguyen.me>
2023-11-05 15:39:42 -0600

    chore(compiler): improve settings for TSConfig + Vite (#87)
    - TSConfig: Don't compile Storybook files
    - TSConfig: set moduleResolution to Bundler to let Vite handle it
    - Vite: Make peer dependencies external from build via Rollup
    - Vite: Configure global variables with React
    - Vite: Configure TailwindCSS as PostCSS plugin
    - chore: use `node:` imports for path module (see: https://nodejs.org/api/esm.html#node-imports)
    - tests: add regression tests for compiler settings
    - ci: add job for running tests

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,7 +1,11 @@
+import path from 'node:path';
+
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import tailwindPlugin from 'tailwindcss';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+
+import { peerDependencies } from './package.json';
 
 export default defineConfig({
 	build: {
@@ -10,6 +14,26 @@ export default defineConfig({
 			name: 'sylon',
 			formats: ['es'],
 			fileName: (format) => `sylon.${format}.js`,
+		},
+		rollupOptions: {
+			external: [
+				...Object.keys(peerDependencies),
+				'react/jsx-runtime',
+				'tailwindcss',
+			],
+			output: {
+				globals: {
+					react: 'React',
+					'tailwindcss': 'tailwindcss',
+				},
+			},
+		},
+	},
+	css: {
+		postcss: {
+			plugins: [
+				tailwindPlugin(),
+			],
 		},
 	},
 	plugins: [
