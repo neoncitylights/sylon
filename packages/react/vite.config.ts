333d5b81c3ecbad974cae645e81613ba4e2c9d30
Samantha <contact@samanthanguyen.me>
2023-11-05 14:29:13 -0600

    chore: use `node:` imports for path module
    see: https://nodejs.org/api/esm.html#node-imports

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,5 +1,6 @@
+import path from 'node:path';
+
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import tailwindPlugin from 'tailwindcss';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
